d2811dfbb1689297881c1af4d6754dfa6b2b630a
Md. Nabobi Hasan <shatin.hasan73@gmail.com>
2023-08-03 12:19:02 +0600

    model, migration and seeder files initialize

--- database/migrations/2023_08_03_061508_create_cards_table.php
+++ database/migrations/2023_08_03_061508_create_cards_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('companies', function (Blueprint $table) {
+        Schema::create('cards', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('companies');
+        Schema::dropIfExists('cards');
     }
 };
